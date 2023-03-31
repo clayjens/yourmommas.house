05045729b3fb222743c5bbb3b02047e647373c31
Clayton Jensen <clayton.jensen@yourmommas.house>
2023-03-31 18:33:07 -0500

    add resume and fill in placeholders

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -1,23 +1,30 @@
 <script lang="ts">
     import { DownloadCloudIcon, MailIcon } from "svelte-feather-icons";
 
-    function handleDownload() {
-        alert('Sadly this feature is not implemented yet :(')
-    }
+    let years_programming = new Date().getFullYear() - 2018;
 </script>
 
 <div class="space-y-4">
-    <h1>Lorem ipsum dolor sit amet</h1>
+    <h1>My Experience</h1>
+    <hr />
+    <h2>Clayton Jensen</h2>
+    <h3>Full-Time Student</h3>
     <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi delectus dolor eveniet, quasi in quisquam quam doloribus quia, fuga, sapiente molestiae natus exercitationem debitis numquam laudantium. Amet, sit optio! Nihil?
-        Quaerat provident blanditiis unde porro, facere sed eaque dolore accusamus enim, ut eius dolorum ducimus veritatis laborum consectetur eos possimus? Odio quo optio neque nulla officia adipisci explicabo alias architecto.
+        I am a motivated student working towards degrees in Computer Science & Information Technology with a minor in Mathematics and a certificate in programming.
+
+        I am contributing to Machine Learning-based research and developing my skills in producing Artificial Intelligence models.
+
+        I am a skilled programmer with {years_programming} years of experience in software development, primarily in the C++ and Rust programming languages.
+        I also have experience with developing websites.
     </p>
-    <button class="btn variant-filled" on:click={handleDownload}>
-        <DownloadCloudIcon />
-        <span>Download Clayton's Resume</span>
-    </button>
-    <a class="btn variant-filled" href="mailto:clayton.jensen@yourmommas.house">
-        <MailIcon />
-        <span>Send An Email</span>
-    </a>
+    <div class="space-x-2">
+        <a class="btn variant-filled" href="./Clayton_Jensen__Resume.pdf" download="resume-Clayton_Jensen.pdf">
+            <DownloadCloudIcon />
+            <span>Download Clayton's Resume</span>
+        </a>
+        <a class="btn variant-filled" href="mailto:clayton.jensen@yourmommas.house">
+            <MailIcon />
+            <span>Send An Email</span>
+        </a>
+    </div>
 </div>
