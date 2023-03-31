ca4698b10cdec8352b01996f092524f47dd8a500
Clayton Jensen <clayton.jensen@yourmommas.house>
2023-03-31 00:58:51 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { GithubIcon, LinkedinIcon, TwitterIcon } from "svelte-feather-icons";
+</script>
+
+<svelte:head>
+	<title>Clayton Jensen</title>
+</svelte:head>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1>Hi, I'm Clayton</h1>
+		<h3>I'm a Computer Science major focusing in software engineering and machine learning.</h3>
+		<p>Currently, this website is a placeholder for more to come...</p>
+		<hr />
+		<h4>In the meantime, why not check out my socials?</h4>
+		<div>
+			<a class="btn variant-filled" href="https://github.com/clayjens">
+				<GithubIcon/>
+			</a>
+			<a class="btn variant-filled" href="https://www.linkedin.com/in/clayton-jensen-875556219/">
+				<LinkedinIcon/>
+			</a>
+			<a class="btn variant-filled" href="/" on:click={() => alert("My Twitter isn't linked, maybe one day!")}>
+				<TwitterIcon/>
+			</a>
+		</div>
+		<code>I'm currently working on my LinkedIn profile</code>
+		<hr />
+		<h4>Why is it called Your Momma's House?</h4>
+		<p>It's a long story, but essentially I thought it was funny and provocative.</p>
+		<p>Also, yourmommas.house is linked to a Google Workspace, so I can use funny email names like <a href="mailto:clayton.jensen@yourmommas.house">clayton.jensen@yourmommas.house</a>. Go ahead and send me an email!</p>
+	</div>
+</div>
